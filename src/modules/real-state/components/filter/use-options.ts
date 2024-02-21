58f5e68528be65bb32c0510e96bc0228b019e81e
jvitormelo <jvitor.ovidio2@hotmail.com>
2024-02-21 13:34:32 -0300

    Refactor use-options.ts to create unique options for bedrooms, bathrooms, and parking

--- src/modules/real-state/components/filter/use-options.ts
+++ src/modules/real-state/components/filter/use-options.ts
@@ -1,35 +1,30 @@
 import { allRealEstateData } from "@/modules/real-state/api/get-real-state";
 
-function fillNumberOptions(data: number[]) {
-  // get the min and max values
+function createUniqueOptions(data: number[]): string[] {
+  const uniqueOptions = new Set(data);
 
-  const min = Math.min(...data);
-  const max = Math.max(...data);
-
-  // create a new array with the range of values
-  const range = Array.from({ length: max - min + 1 }, (_, i) => i + min);
-
-  // create the options
-  return range.map(String);
+  return Array.from(uniqueOptions, String).toSorted();
 }
 
-const bedroomsOptions = fillNumberOptions(
+const bedroomsOptions = createUniqueOptions(
   allRealEstateData.map((realEstate) => realEstate.bedrooms)
 );
-const bathroomsOptions = fillNumberOptions(
+
+const bathroomsOptions = createUniqueOptions(
   allRealEstateData.map((realEstate) => realEstate.bathrooms)
 );
-const parkingOptions = fillNumberOptions(
+
+const parkingOptions = createUniqueOptions(
   allRealEstateData.map((realEstate) => realEstate.parking)
 );
 
-const maxPrice = Math.max(
-  ...allRealEstateData.map((realEstate) => realEstate.salePrice)
+const salePriceMap = allRealEstateData.map(
+  (realEstate) => realEstate.salePrice
 );
 
-const minPrice = Math.min(
-  ...allRealEstateData.map((realEstate) => realEstate.salePrice)
-);
+const maxPrice = Math.max(...salePriceMap);
+
+const minPrice = Math.min(...salePriceMap);
 
 export function useRealStateFilterOptions() {
   return {
