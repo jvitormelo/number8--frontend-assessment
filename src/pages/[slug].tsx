ffe365c84790734400bb3bdb4ddaba1ad80f2f7a
jvitormelo <jvitor.ovidio2@hotmail.com>
2024-02-20 23:30:28 -0300

    Creating Page Structure

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -2,16 +2,19 @@ import { getRealEstate } from "@/modules/real-state/api/get-real-state";
 import { RealEstate } from "@/modules/real-state/types";
 import { formatDollar } from "@/utils/currency";
 import {
+  Box,
   Card,
   Grid,
   GridCol,
   Group,
+  Image,
   SimpleGrid,
   Stack,
   Text,
   Title,
 } from "@mantine/core";
 import { GetStaticPaths, GetStaticProps } from "next";
+import ImageNext from "next/image";
 
 type Params = {
   slug: string;
@@ -49,20 +52,61 @@ export const getStaticProps: GetStaticProps<Props> = async (context) => {
 const RealEstateDetail = ({ realEstate }: Props) => {
   return (
     <Grid>
-      <GridCol span={8}>
-        <Group justify={"space-between"} component={"header"}>
-          <Stack>
-            <Title order={1}>{realEstate.title}</Title>
-            <Text>{realEstate.location}</Text>
-          </Stack>
-          <Stack>
-            <Title order={2}>{formatDollar(realEstate.salePrice)}</Title>
-            <Text c={"gray"}>{realEstate.dateListed}</Text>
-          </Stack>
-        </Group>
+      <GridCol span={{ xs: 12, md: 8 }}>
+        <Stack>
+          <Group justify={"space-between"} component={"header"}>
+            <Stack>
+              <Title order={1}>{realEstate.title}</Title>
+              <Text>{realEstate.location}</Text>
+            </Stack>
+            <Stack>
+              <Title order={2}>{formatDollar(realEstate.salePrice)}</Title>
+              <Text c={"gray"}>{realEstate.dateListed}</Text>
+            </Stack>
+          </Group>
+          <Box pos={"relative"} h={"400px"}>
+            <ImageNext
+              src={realEstate.pictureURL}
+              alt={realEstate.title}
+              fill
+              sizes="(max-width: 768px) 100vw,  70vw"
+            />
+          </Box>
+
+          <Card withBorder p={"lg"}>
+            <Group justify="space-between">
+              <Stack gap={0} align="center">
+                <Text size="xl" fw={"bold"}>
+                  {realEstate.bedrooms}
+                </Text>
+                <Text>Bed</Text>
+              </Stack>
+              <Stack>
+                <Title order={3}>{realEstate.bathrooms}</Title>
+                <Text>Bath</Text>
+              </Stack>
+              <Stack>
+                <Title order={3}>{realEstate.parking}</Title>
+                <Text>Parking</Text>
+              </Stack>
+
+              <Stack>
+                <Title order={3}>{realEstate.sqft}</Title>
+                <Text>SQFT</Text>
+              </Stack>
+
+              <Stack>
+                <Title order={3}>{realEstate.yearBuilt}</Title>
+                <Text>Year Built</Text>
+              </Stack>
+            </Group>
+          </Card>
+
+          <Text>{realEstate.description}</Text>
+        </Stack>
       </GridCol>
 
-      <GridCol span={4}>
+      <GridCol span={{ xs: 12, md: 4 }}>
         <Stack>
           <Card withBorder h={"full"}></Card>
         </Stack>
